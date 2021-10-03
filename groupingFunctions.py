1aa6b079014ec30fc4824b3c0c1a6888e757341e
Sarah Poulter <spou548@aucklanduni.ac.nz>
2021-10-04 11:31:03 +1300

    groupingFunctions updated and docstrings added

--- groupingFunctions.py
+++ groupingFunctions.py
@@ -5,13 +5,30 @@ from pulp import *
 from routeFunctions import *
 
 def CreateGroups(distanceData,locationData,startingNodes):
-    
-    # initialising number of nearest nodes
-    k = 15
+    ''' Groups the nodes based on proximity to selected starting nodes.
+
+        Parameters:
+        -----------
+        locationData : pandas dataframe
+                        dataframe containing location data.
+        distanceData : pandas dataframe
+                        dataframe containing travel time data.
+        startingNodes : array
+                    string array of starting nodes for each group
+        Returns:
+        --------
+        locationData : pandas dataframe
+                        dataframe containing location data updated with additional grouping. 
+        Notes:
+        --------
+        Calls the assignNode function 
+    '''
+    # initialising number of number of nodes selected for each group 
+    k = 10 
 
     # loop through starting nodes
     for node in startingNodes:
-        group = [0,0,0,0,0,0]
+        group = np.zeros(len(startingNodes))
         group[startingNodes.index(node)] = 1
         # extracting the row of distances from the starting node
         distances = distanceData.loc[node,:]
@@ -21,21 +38,11 @@ def CreateGroups(distanceData,locationData,startingNodes):
         closestNodeDistances = distances[:k]
         # extracting names of closest nodes
         for i in closestNodeDistances.index:
-            assignNode(locationData, i, group=group)
-pass
+            assignNode(locationData, i, group)
 
-def assignNode(locationData, location, group = [0,0,0,0,0,0]):
-
-    for i in range(len(group)):
-        if (group[i] == 1):
-            locationData.loc[locationData['Store'] == location, 'Group '+str(i+1)] = 1
-    
-    return locationData
-
-def checkNodeAssignment(locationData,distanceData,startingNodes):
     # loop through each of the nodes to check that it has been assigned to at least one group
     for node in locationData.index:
-        groups = locationData.loc[node,'Group 1':'Group 6']
+        groups = locationData.loc[node,'Group 1':'Group '+ str(len(startingNodes))]
         if (groups.sum() < 1):
             # extracting the row of distances from the starting node
             distances = distanceData.loc[distanceData.index[node],startingNodes]
@@ -44,11 +51,29 @@ def checkNodeAssignment(locationData,distanceData,startingNodes):
             # selection the k smallest distances
             closestNodeDistance = distances[:1]
             # Extract the group number for the closest node group
-            group = [0,0,0,0,0,0]
+            group = np.zeros(len(startingNodes))
             group[startingNodes.index(closestNodeDistance.index)] = 1
             # assign to closest group 
             assignNode(locationData, distanceData.index[node], group=group)
 
+    return locationData
+
+
+def assignNode(locationData, location, group):
+    ''' Assigns a specific location node to an indicated group.
+
+        Parameters:
+        -----------
+        locationData : pandas dataframe
+                        dataframe containing location data.
+        location : string 
+                    name of the node being assigned.
+        group : array
+                    binary array indicating which group the node is being assigned to.
+    '''
+    for i in range(len(group)):
+        if (group[i] == 1):
+            locationData.loc[locationData['Store'] == location, 'Group '+str(i+1)] = 1
 
 
 if __name__ == "__main__":
@@ -57,21 +82,17 @@ if __name__ == "__main__":
     distanceData = pd.read_csv('WoolworthsDistances.csv', index_col=0)
     locationData = pd.read_csv('WoolworthsLocations.csv')
 
-    # Initializing grouping columns,
-    locationData['Group 1'] = 0
-    locationData['Group 2'] = 0
-    locationData['Group 3'] = 0
-    locationData['Group 4'] = 0
-    locationData['Group 5'] = 0
-    locationData['Group 6'] = 0
-
     # Create a list of starting nodes to create groups around based of geographical intuition
     startingNodes = ['Countdown Lynfield','Countdown Manukau Mall','Countdown Henderson','Countdown Highland Park','Countdown Mt Eden','Countdown Milford']
 
-    CreateGroups(distanceData,locationData,startingNodes)
+    # Initializing grouping columns
+    for i in range(len(startingNodes)):
+        locationData['Group '+ str(i+1)] = 0
+    
+    # Apply grouping function
+    GroupedLocations = CreateGroups(distanceData,locationData,startingNodes)
 
-    checkNodeAssignment(locationData,distanceData,startingNodes)
-
-    locationData.to_csv('GroupedLocations.csv')
+    # Export grouping information to csv
+    GroupedLocations.to_csv('GroupedLocations.csv')
 
 
