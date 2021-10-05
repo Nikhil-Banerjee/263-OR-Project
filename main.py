3c6086df3244cab2340be6f898fa4a994134a833
LabJared <80793413+LabJared@users.noreply.github.com>
2021-10-06 03:05:59 +1300

    Added import from routeFuncsNikhil and removed subsequent redundancies.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import math
 import pickle
 from pulp import *
+from routeFuncsNikhil import *
 
 travelTimes = pd.read_csv('WoolworthsTravelDurations.csv', index_col=0)
 
@@ -33,8 +34,7 @@ def main():
         satR = pickle.load(f)
     with open('savedSatTimes.pkl', 'rb') as f:
         satTimes = pickle.load(f)
-    wkDayR=hardcode()
-    wkDayTimes=[0]*30
+
     wkDayRoutes=matrixForm(wkDayR, stores)
     satRoutes=matrixForm(satR, stores)
     
@@ -58,7 +58,7 @@ def main():
         probWkDay += lpSum([wkDay_route_vars[i] * wkDayRoutes[i][store] for i in wkDayRoutes]) >= 1
     for route in range(wkDayRoutes.shape[1]):
         # Truck capacity is not surpassed for any route
-        #probWkDay += lpSum([wkDayDemand[store]*wkDay_route_vars[route]*wkDayRoutes[route][store] for store in stores])   <= truckCapacity
+        probWkDay += lpSum([wkDayDemand[store]*wkDay_route_vars[route]*wkDayRoutes[route][store] for store in stores])   <= truckCapacity
         #  4 hour time limit is not surpassed for any route
         probWkDay += lpSum(calculateRouteTime(wkDayR[route])/60+lpSum([unloadTime*wkDayDemand[store]*wkDay_route_vars[route]*wkDayRoutes[route][store] for store in stores])) <= maxRouteTime
     
@@ -133,32 +133,6 @@ def averageDemand(demandData, day):
     return averages
 
 
-def matrixForm(Routes, stores):
-    ''' Creates a binary matrix of stores visited by each route
-        Parameters:
-        -----------
-        Routes : list of string lists
-                    string lists containing stores visited by a given route
-        stores : string list
-                    List of all stores in the network
-                        - Should not include the distribution centre
-        Returns:
-        --------
-        RoutesMatrix : pandas dataframe 
-                        A binary matrix of whether each store was visited by a given route 
-    '''
-    RoutesDict={}
-
-    for Routeindex in range(len(Routes)):
-        Nodes = pd.Series([0]*len(stores), index=stores) # Initialise with zeros for each iteration
-        for stop in Routes[Routeindex][1::]:             # Ignore the distribution centre
-            Nodes[stop] = 1
-        RoutesDict[Routeindex] = Nodes
-    
-    RoutesMatrix = pd.DataFrame(RoutesDict)
-    return RoutesMatrix
-
-
 def loadDemands():
     data = pd.read_csv('WoolworthsDemands.csv')
     
@@ -185,61 +159,6 @@ def loadDemands():
 
     return data
 
-
-def calculateRouteTime(route):
-    ''' 
-        Calculates the route travel time for a given route.
-        The route is assumed to be a round trip and thus after visiting the last location, a return to start location is assumed
-        Parameters:
-        -----------
-        route : string list
-                string list containing locations to visit in route.
-                - The route is assumed to be a round trip and thus after visiting the last location, a return to start location is assumed
-        travelTimes : pandas dataframe
-                        given data on travel durations.
-                        - use the default input.
-        
-        Returns:
-        --------
-        time : float
-                Route time
-    '''
-
-    time = 0
-
-    for i in range(len(route)):
-        if (i == len(route) - 1):
-            time += tripTime(route[i], route[0])
-
-        else:
-            time += tripTime(route[i], route[i+1])
-        
-        if(time > 14400): return np.inf # Exits loops if current route time over 4 hours and returns infinity if so.
-
-
-    return time
-
-def tripTime(source, destination):
-    ''' Returns trip time from source to destination.
-        Inputs:
-        ------
-        travelTimes : pandas dataframe
-                        dataframe containing travel durations.
-        source      : string
-                        string of source name.
-        destination : string
-                        string of destination name.
-        
-        Returns:
-        --------
-        tripTime : float
-                    trip time
-    '''
-    if (source == destination):
-        ValueError('Source and Destination are the same.')
-
-    tripTime = travelTimes.loc[source][destination]
-    return tripTime
 
 def hardcode():
     routes = []
