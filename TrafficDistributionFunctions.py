f59ee31bb7f23930391cffdfa310b122d897a123
mathijsfijma <80793610+mathijsfijma@users.noreply.github.com>
2021-10-15 18:47:37 +1300

    added docstrings/comments

--- TrafficDistributionFunctions.py
+++ TrafficDistributionFunctions.py
@@ -4,35 +4,82 @@ import pandas as pd
 from random import betavariate
 
 def get_mult(traffic):
+    '''
+    Generates a random variate from normal distribution 
 
+    Inputs:
+        traffic (Pandas Dataframe)
+            - dataframe containing traffic congestion data
+    Outputs:
+        dist (Integer)
+            - random variate from normal distribution
+    '''
     dist = np.random.normal(np.std(traffic))
 
     return dist
 
 def pert(a,b,c,*, lamb=4):
-    
+    '''
+    Generates a random variate from pert distribution 
+
+    Inputs:
+        a (Integer)
+            - min in data set
+        b (Integer)
+            - average of data set
+        c (Integer)
+            - max in data set
+    '''
     r = c - a
     alpha = 1 + lamb * (b - a) / r
     beta = 1 + lamb * (c - b) / r
     return a + betavariate(alpha, beta) * r
 
 def findmax(list):
+    '''
+    Finds max value in dataframe
+
+    Inputs:
+        list (numpy 2D array)
+            - numpy 2D array containing data values
+    Outputs:
+        max (Integer)
+            - max value in 2D numpy array
+    '''
+
     max = list[0][0]
     for i in range(0,list.shape[0]):
         for j in range(0,list.shape[1]):
-            if list[i][j] < max:
+            if list[i][j] > max:
                 max = list[i][j]
     return max
 
 def findmin(list):
+    '''
+    Finds min value in dataframe
+
+    Inputs:
+        list (numpy 2D array)
+            - numpy 2D array containing data values
+    Outputs:
+        min (Integer)
+            - min value in 2D numpy array
+    '''
     min = list[0][0]
     for i in range(0,list.shape[0]):
         for j in range(0,list.shape[1]):
-            if list[i][j] > min:
+            if list[i][j] < min:
                 min = list[i][j]
     return min
 
 def findavg(list):
+    '''
+    Finds avg value in dataframe
+
+    Inputs:
+        list (numpy 2D array)
+            - numpy 2D array containing data values
+    '''
     sum = 0
     count = 0
     for i in range(0,list.shape[0]):
@@ -40,8 +87,10 @@ def findavg(list):
                 sum += list[i][j]
                 count += 1
     return sum/count
+
 if __name__ == "__main__":
     mults = []
+    #Testing code
     congestion = pd.read_csv('AKL_congestion.csv', index_col=0)
 
     weekday_8am = (congestion[["8am","9am","10am","11am"]]).loc["Mon":"Fri"]
